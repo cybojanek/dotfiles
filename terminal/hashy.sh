dd6f577b56c1f196c28149b2abc50d55f46f8502
Jan Kasiak <j.kasiak@gmail.com>
2014-01-03 00:38:15 -0500

    Launch hashy only on interactive shell

--- terminal/hashy.sh
+++ terminal/hashy.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
-MD5="md5sum"
-if [[ `uname` == 'Darwin' ]]; then MD5="md5" ; fi
-HASH=`hostname -s | ${MD5}`
-echo -n -e     "\033]6;1;bg;red;brightness;$((0x${HASH:0:2}))\a\033]6;1;bg;green;brightness;$((0x${HASH:2:2}))\a\033]6;1;bg;blue;brightness;$((0x${HASH:4:2}))\a"
+if [ -z "$PS1" ]; then
+
+else
+    MD5="md5sum"
+    if [[ `uname` == 'Darwin' ]]; then
+        MD5="md5"
+    fi
+    HASH=`hostname -s | ${MD5}`
+    echo -n -e "\033]6;1;bg;red;brightness;$((0x${HASH:0:2}))\a\033]6;1;bg;green;brightness;$((0x${HASH:2:2}))\a\033]6;1;bg;blue;brightness;$((0x${HASH:4:2}))\a"
+fi
